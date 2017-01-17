7d68a1dc6dfb176c0a9ff961264f2c0c28ad1650
360flipdor <360flipdor@gmail.com>
2017-01-17 13:25:29 +0100

    serial kommunikasjon fungere sån halveis ikke stress det liksom

--- serial_kom.py
+++ serial_kom.py
@@ -1,46 +1,76 @@
 import serial
-import queue
-import time
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinter import *
+from tkinter import ttk
+import tkinter
 import threading
 
-# Installering av variabler
-kommando = '0'
-brukarkommandoar = queue.Queue()
-connected = True
-port = 'COM10'
-baud = 115200 #115200  # 9600
 
-data = []
 
-serieport = serial.Serial(port, baud, timeout=1)
 
-if serieport.isOpen():
-    print(serieport.name, 'er open')
-else:
-    serieport.open()
+def lesing_arduino():
+    # Installering av variabler
+    connected = True
+    port = 'COM13'
+    baud = 9600  # 115200  # 9600
 
-def print_data():
+    serieport = serial.Serial(port, baud, timeout=1)
+
+    if serieport.isOpen():
+        print(serieport.name, 'er open')
+    else:
+        serieport.open()
+
+    x = []
+    data = []
+    telling = 0
+    a = []
+    b = 0
+    runde = 0
+
     while(1):
-        teikn = serieport.read(1)
-        #teikn = str(serieport.read(1), encoding='utf-8')  # Les eitt teikn.  #KT La til convert til str
-        print(teikn)
-serieport.write('k'.encode('utf-8'))
-serieport.write('k'.encode('utf-8'))
-starting = threading.Thread(target=print_data)
-starting.start()
+        #teikn = serieport.read(1)
+        teikn = str(serieport.read(1), encoding='utf-8')  # Les eitt teikn.  #KT La til convert til str
+        #print(teikn)
+        a.append(teikn)
 
-while(1):
-    aa=0
 
-    serieport.write('k'.encode('utf-8'))
+        if (teikn == "F"):
+            l1 = [a[b-3],a[b-4],a[b-5]]
+            s = ''.join(l1)
 
-    #serieport.write('k'.encode('utf-8'))
+            data.append(s)
+            x.append(runde)
+            print(data[runde])
+            runde += 1
 
-    #time.sleep(3)
+            avstand_graf.clear()
+            avstand_graf.plot(x,data,'r')
 
-    #serieport.write('s'.encode('utf-8'))
 
-    #time.sleep(3)
+
+        b += 1
+
+
+lesing = threading.Thread(target=lesing_arduino)
+lesing.start()
+
+figur_test = Figure(figsize=(5, 5), dpi=70)
+figur_test.add_axes()
+avstand_graf = figur_test.add_subplot(111)
+
+root = Tk()                           # Lager et vindu
+root.title("Gruppe 2 Analyse av data")# setter navn på vinduet
+
+Figure = FigureCanvasTkAgg(figur_test,root)
+Figure.show()
+Figure.get_tk_widget().grid(column=0, row=0)
+
+root.mainloop()
+
+
+
 
 
 
